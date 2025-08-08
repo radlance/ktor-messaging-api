e299e767cfb07e9c049af378eaa65c8bc2840aab
Dmitry <manyakindima@gmail.com>
2025-08-08 16:42:19 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "ktor-messaging-api"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/com/github/radlance/ktormessagingapi/Application.kt
@@ -0,0 +1,22 @@
+package com.github.radlance.ktormessagingapi
+
+import com.github.radlance.ktormessagingapi.plugins.configureDatabases
+import com.github.radlance.ktormessagingapi.plugins.configureMonitoring
+import com.github.radlance.ktormessagingapi.plugins.configureRouting
+import com.github.radlance.ktormessagingapi.plugins.configureSecurity
+import com.github.radlance.ktormessagingapi.plugins.configureSerialization
+import com.github.radlance.ktormessagingapi.plugins.configureSockets
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureMonitoring()
+    configureSockets()
+    configureSecurity()
+    configureRouting()
+}
